96f8d91b7221bd29dc372c9eb16a0f5eaba300f7
Lourival Sabino <lourival.sabino.junior@gmail.com>
2020-07-29 17:40:33 -0300

    Scripts de build

--- source/desafio-otimo-ui/build.ps1
+++ source/desafio-otimo-ui/build.ps1
@@ -1,6 +1,6 @@
-#ng build
+ng build
 rm -Recurse ..\..\docker\images\nginx\html\*.html
 rm -Recurse ..\..\docker\images\nginx\html\*.ico
 rm -Recurse ..\..\docker\images\nginx\html\*.js
 rm -Recurse ..\..\docker\images\nginx\html\*.js.map
-#cp dist\desafio-otimo-ui\* ..\..\docker\images\nginx\html
+cp dist\desafio-otimo-ui\* ..\..\docker\images\nginx\html
